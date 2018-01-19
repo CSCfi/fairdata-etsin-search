ace6d6ef518b9421170660375a5860a0c7ec580f
Juha-Matti Lehtinen <junnu.lehtinen@gmail.com>
2018-01-19 13:35:06 +0200

    CSCETSIN-130: [FIX] Flake8

--- tests/test_catalog_record_converter.py
+++ tests/test_catalog_record_converter.py
@@ -6,4 +6,3 @@ def test_get_es_person_or_org_common_data_from_metax_obj():
     converter = CRConverter()
     output = converter.convert_metax_cr_json_to_es_data_model(get_test_object_from_file('metax_catalog_record.json'))
     assert output == get_test_object_from_file('es_document.json')
-
