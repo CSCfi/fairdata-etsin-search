3069f30eaa5dee213f4f384acde68dcb686a17a3
Juha-Matti Lehtinen <junnu.lehtinen@gmail.com>
2018-06-15 11:39:34 +0300

    CSCETSIN-123: [ADD] License information

--- delete_index.py
+++ delete_index.py
@@ -1,3 +1,10 @@
+# This file is part of the Etsin service
+#
+# Copyright 2017-2018 Ministry of Education and Culture, Finland
+#
+# :author: CSC - IT Center for Science Ltd., Espoo Finland <servicedesk@csc.fi>
+# :license: MIT
+
 from etsin_finder_search.reindexer import delete_search_index
 
 
--- etsin_finder_search/catalog_record_converter.py
+++ etsin_finder_search/catalog_record_converter.py
@@ -1,3 +1,10 @@
+# This file is part of the Etsin service
+#
+# Copyright 2017-2018 Ministry of Education and Culture, Finland
+#
+# :author: CSC - IT Center for Science Ltd., Espoo Finland <servicedesk@csc.fi>
+# :license: MIT
+
 from etsin_finder_search.reindexing_log import get_logger
 from etsin_finder_search.utils import \
     catalog_record_has_preferred_identifier, \
