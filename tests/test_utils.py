8a7a0b8634f52add301d50319297947968cd0464
Jori Niemi <3295718+tahme@users.noreply.github.com>
2021-03-30 12:48:17 +0300

    CSCFAIRMETA-977: Allow only published datasets when indexing/reindexing

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,14 +1,31 @@
 # This file is part of the Etsin service
 #
-# Copyright 2017-2018 Ministry of Education and Culture, Finland
+# Copyright 2017-2021 Ministry of Education and Culture, Finland
 #
 # :author: CSC - IT Center for Science Ltd., Espoo Finland <servicedesk@csc.fi>
 # :license: MIT
 
-import json
-import os
+import pytest
 
+from etsin_finder_search.utils import catalog_record_should_be_indexed
+from .helpers import get_test_object_from_file
 
-def get_test_object_from_file(filename):
-    json_data = open('{0}/test_objects/{1}'.format(os.path.dirname(os.path.realpath(__file__)), filename)).read()
-    return json.loads(json_data)
+@pytest.fixture
+def cr():
+    return get_test_object_from_file('metax_catalog_record.json')
+
+class TestCatalogRecordShouldBeIndexed:
+    def test_published_record_should_be_indexed(self, cr):
+        assert catalog_record_should_be_indexed(cr)
+
+    def test_record_without_catalog_id_should_not_be_indexed(self, cr):
+        del cr['data_catalog']['catalog_json']['identifier']
+        assert not catalog_record_should_be_indexed(cr)
+
+    def test_record_in_legacy_catalog_should_not_be_indexed(self, cr):
+        cr['data_catalog']['catalog_json']['identifier'] = 'urn:nbn:fi:att:data-catalog-legacy'
+        assert not catalog_record_should_be_indexed(cr)
+
+    def test_draft_record_should_not_be_indexed(self, cr):
+        cr['state'] = 'draft'
+        assert not catalog_record_should_be_indexed(cr)
